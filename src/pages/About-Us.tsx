77ad9ba4a9c24845c22f4acefc5151da48479250
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-06-02 20:55:08 -0400

    small changes ot about me section

--- src/pages/About-Us.tsx
+++ src/pages/About-Us.tsx
@@ -12,20 +12,20 @@ const AboutMe: React.FC = () => {
       </p>
       <h2>What We Do</h2>
 <h3>Web Design</h3>
-<p>Design that Delivers.
-We craft visually striking, user-friendly websites that reflect your brand’s identity and drive real engagement.</p>
+<h5>Design that Delivers.</h5>
+<p> We craft visually striking, user-friendly websites that reflect your brand’s identity and drive real engagement.</p>
 
 <h3>Web Development</h3>
-<p>Scalable. Secure. Seamless.
-Our custom-built websites and applications are developed with clean code, modern tech stacks, and performance in mind — ready to grow with your business.</p>
+<h5>Scalable. Secure. Seamless.</h5>
+<p> Our custom-built websites and applications are developed with clean code, modern tech stacks, and performance in mind — ready to grow with your business.</p>
 
 <h3>SEO & Optimization</h3>
-<p>Be Seen. Be Found. Be Ahead.
-We implement strategic SEO and performance optimizations to boost your visibility, rankings, and page speed — turning traffic into results.</p>
+<h5>Be Seen. Be Found. Be Ahead.</h5>
+<p> We implement strategic SEO and performance optimizations to boost your visibility, rankings, and page speed — turning traffic into results.</p>
 
 <h3>Brand Identity</h3>
-<p>Make Your Mark.
-We help you define a bold, consistent brand presence that connects with your audience and stands out in a crowded digital space.</p>
+<h5>Make Your Mark.</h5>
+<p> We help you define a bold, consistent brand presence that connects with your audience and stands out in a crowded digital space.</p>
 
     </section>
   );
